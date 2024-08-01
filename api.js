039fa49f1b978fd520918d693645bc947786c87a
Lesego210 <127973618+Lesego210@users.noreply.github.com>
2024-08-01 21:03:43 +0200

    api.js

--- api.js
+++ api.js
@@ -1,7 +1,1 @@
-// src/services/api.js
-import axios from 'axios';
 
-const API_BASE_URL = 'https://podcast-api.netlify.app';
-
-export const fetchShows = () => axios.get(`${API_BASE_URL}/shows`);
-export const fetchShowById = (id) => axios.get(`${API_BASE_URL}/id/${id}`);
