8fcc4c497a1de5a8ccc350d4ffebda2b6792202f
Lesego210 <127973618+Lesego210@users.noreply.github.com>
2024-08-01 20:51:06 +0200

    Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,34 +1,58 @@
-// src/pages/Login.jsx
-import React, { useState } from 'react';
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useNavigate } from 'react-router-dom';
+import { useCallback, useEffect } from 'react';
 
-const Login = () => {
-  const [email, setEmail] = useState('yvonne.mathole.ym@gmail.com');
-  const [password, setPassword] = useState('Lethabo@22');
+
+const  supabaseUrl = 'https://mkayoundsyjpvbejcvnh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rYXlvdW5kc3lqcHZiZWpjdm5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk1MzkwNTgsImV4cCI6MjAxNTExNTA1OH0.RVVkcFxkfycYdSwoAfkONPmaDfgzAd-9DVZUHWn13ps'; // Replace with your actual Supabase API key
+
+const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
+const LoginPage = () => {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Implement your authentication logic here
-    // For simplicity, assuming login is always successful
-    navigate('/');
-  };
+  const handleAuthStateChange = useCallback(
+    async (event) => {
+      if (event == 'SIGNED_OUT') {
+        navigate('/');
+      } else if (event == 'SIGNED_IN') {
+        navigate('/podcasts');
+      }
+    },
+    [navigate]
+  );
+
+  useEffect(() => {
+    // Set up the onAuthStateChange listener with the callback function
+    supabase.auth.onAuthStateChange(handleAuthStateChange);
+
+  
+
+    // Cleanup when the component unmounts
+    return () => {
+      // Perform any necessary cleanup here
+    };
+  }, [handleAuthStateChange]);
 
   return (
     <div>
-      <h1>Login</h1>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+
+
+      <h2 className='topHeader'>
+        Podcast
+      </h2>
+      <Auth
+    supabaseClient={supabase}
+    
+    appearance={{ theme: ThemeSupa }}
+  />
     </div>
   );
 };
 
-export default Login;
+export default LoginPage;
